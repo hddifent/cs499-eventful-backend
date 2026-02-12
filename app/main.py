45c3b2cb57075a07b11acded02cbdc5b0c33193b
hddifent <preukwork@gmail.com>
2026-02-12 13:00:32 +0700

    Added mock events fetching API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from app.api import process_map
+from app.api import process_map, events
+
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 app = FastAPI()
 
 app.include_router(process_map.router, prefix="/api/map")
+app.include_router(events.router, prefix="/api/events")
