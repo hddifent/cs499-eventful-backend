0749d5d89fb981e339a59438e7c56763fafe8d40
hddifent <preukwork@gmail.com>
2026-03-04 14:54:40 +0700

    Create simple schemas

---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+# -- Users --
+class UserBase(BaseModel): # Not sure what to do yet...
+    username: str = Field(min_length=1, max_length=32)
+    user_display_name: str = Field(min_length=1, max_length=100)
+
+class UserCreate(UserBase):
+    user_email: EmailStr
+    user_pwd: str = Field(min_length=8, max_length=64)
+
+# TODO: Mocking a lot of stuffs rn. Fix me pls.
+# -- Events --
+class EventBase(BaseModel):
+    title: str = Field(min_length=1, max_length=256)
+    description: str = Field(default="")
