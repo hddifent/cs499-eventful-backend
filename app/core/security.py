5bb7237d3f1085d7844151b1a3b0e0a81e9ccf78
hddifent <preukwork@gmail.com>
2026-03-06 17:45:20 +0700

    Added session token related functions.

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,7 @@
 import bcrypt
+import secrets
+import hashlib
+import hmac
 
 def hash_password(password: str) -> str:
     pwd_bytes = password.encode('utf-8')
@@ -11,3 +14,15 @@ def verify_password(pwd: str, db_pwd_hash: str) -> bool:
         pwd.encode('utf-8'), 
         db_pwd_hash.encode('utf-8')
     )
+
+def generate_session_tokens() -> tuple[str, str]:
+    token_id = secrets.token_urlsafe(16)
+    token_secret = secrets.token_urlsafe(32)
+    return token_id, token_secret
+
+def hash_session_secret(secret: str) -> str:
+    return hashlib.sha256(secret.encode('utf-8')).hexdigest()
+
+def verify_session_token(token: str, db_token_hash: str) -> bool:
+    token_hash = hash_session_secret(token)
+    return hmac.compare_digest(token_hash, db_token_hash)
