853c92ed63bf846b99282721fe474e301bea482c
hddifent <preukwork@gmail.com>
2026-03-06 17:47:08 +0700

    Adjust DB models and Pydantic schemas

--- app/db/models.py
+++ app/db/models.py
@@ -1,13 +1,34 @@
-from sqlalchemy import Integer, String
+from __future__ import annotations
+from typing import List
+
+from sqlalchemy import ForeignKey, String, DateTime, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.database import Base
 
+from datetime import datetime
+
 class User(Base):
     __tablename__ = "users"
 
-    user_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
     user_email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     user_display_name: Mapped[str] = mapped_column(String, nullable=False)
-    user_pwd: Mapped[str] = mapped_column(String, nullable=False) # Hashed passwords
+    user_pwd: Mapped[str] = mapped_column(String, nullable=False)
+
+    user_sessions: Mapped[List[Session]] = relationship(back_populates="session_user")
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    session_id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    session_secret: Mapped[str] = mapped_column(String, nullable=False)
+    expire_window: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    expire_absolute: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.user_id"),
+        nullable=False
+    )
+
+    session_user: Mapped[User] = relationship(back_populates="user_sessions")
--- app/schemas.py
+++ app/schemas.py
@@ -3,11 +3,14 @@ from pydantic import BaseModel, ConfigDict, Field, EmailStr
 # -- Users --
 class UserBase(BaseModel): # Not sure what to do yet...
     username: str = Field(min_length=1, max_length=32)
-    user_display_name: str = Field(min_length=1, max_length=100)
 
 class UserCreate(UserBase):
     user_email: EmailStr
+    user_display_name: str = Field(min_length=1, max_length=100)
     user_pwd: str = Field(min_length=8, max_length=64)
+
+class UserLogin(UserBase):
+    user_pwd: str = Field(min_length=1)
 
 # TODO: Mocking a lot of stuffs rn. Fix me pls.
 # -- Events --
