deaca65411e996348452e0ab958d69ac85cc408d
hddifent <preukwork@gmail.com>
2026-03-06 23:26:02 +0700

    Add session renewal threshold

--- app/core/config.py
+++ app/core/config.py
@@ -2,15 +2,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PRODUCTION: bool
+
     DB_NAME: str
     DB_HOST: str
     DB_USER: str
     DB_PASS: str
     DB_PORT: int
+    
     STORAGE_ROOT: str
+
     SESSION_TIMEOUT: int
     ABSOLUTE_TIMEOUT: int
+
+    SESSION_RENEWAL_THRESHOLD: int
+
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings() # type: ignore
-# print(settings.model_dump())
